943ca55f01fb85104e2b14199116b72e65d90754
Ludwig Kratzl <kratzl.ludwig@gmail.com>
2021-03-01 15:49:36 +0100

    Add argument

--- src/quadrature/main.cpp
+++ src/quadrature/main.cpp
@@ -14,7 +14,7 @@ using namespace quadrature;
 
 double function(double x)
 {
-    //return -pow(x + 4, 2) + 1;
+    // return -pow(x + 4, 2) + 1;
     return -pow(x, 2) + 4;
 }
 
@@ -45,14 +45,24 @@ void initRomberg(vector<double> &y, int n, double a, double b)
 int main(int argc, char *argv[])
 {
     int variant = argc > 1 ? atoi(argv[1]) : 1;
+    int n       = argc > 2 ? atoi(argv[2]) : 2;
 
-    int    n = 2;
     double a = -2;
     double b = 2;
     double h = (b - a) / n;
 
     double val = 0.0;
-    if(variant != 3 && variant != 2)
+    if(variant == 3)
+    {
+        cout << "integrate with romberg\n";
+        double         max_y = pow(2, n);
+        vector<double> y(max_y);
+
+        initRomberg(y, n, a, b);
+        Romberg sum { a, b, y };
+        val = sum.integrate();
+    }
+    else
     {
         vector<double> y(n + 1);
         initDefault(y, n, a, h);
@@ -63,31 +73,22 @@ int main(int argc, char *argv[])
             TrapecoidalSum sum { a, b, y };
             val = sum.integrate();
         }
+        else if(variant == 2)
+        {
+            cout << "integrate with archimedes\n";
+            vector<double> y(0);
+            Archimedes     sum { a, b, y, n, function };
+            val = sum.integrate();
+        }
         else
         {
+            if(n % 2 != 0)
+                throw std::invalid_argument("simpson sum only with odd count of support points (0,1,..,n)");
+
             cout << "integrate with simpson\n";
             SimpsonSum sum { a, b, y };
             val = sum.integrate();
         }
-    }
-    else if(variant == 2)
-    {
-        cout << "integrate with archimedes\n";
-        vector<double> y(0);
-        int            levels = 5;
-        Archimedes sum { a, b, y, levels, function };
-
-        val = sum.integrate();
-    }
-    else
-    {
-        cout << "integrate with romberg\n";
-        double         max_y = pow(2, n);
-        vector<double> y(max_y);
-
-        initRomberg(y, n, a, b);
-        Romberg sum { a, b, y };
-        val = sum.integrate();
     }
 
     cout << "Integration von a=" << a << " bis b=" << b << " mit n=" << n << " (-> Stützstellen 0,..," << n << ") ist " << val << "\n";
